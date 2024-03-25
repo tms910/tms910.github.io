008c124c2e733f36446e863edafcf909fbb4fa93
tms910 <146007979+tms910@users.noreply.github.com>
2024-03-25 12:10:49 +0200

    Give hover color to divs

--- etch.js
+++ etch.js
@@ -1,8 +1,17 @@
 const container=document.createElement('div');
-            container.setAttribute('name','container');
-            document.body.appendChild(container);
-            for(let i=0; i<16; i++){
-                const div=document.createElement('div');
-                div.textContent='div';
-                container.appendChild(div);
-            }
+container.setAttribute('class','container');
+document.body.appendChild(container);
+const i=1;
+
+for(let i=0; i<16; i++){
+    const div=document.createElement('div');
+    div.setAttribute('class','child');
+    container.appendChild(div);
+    
+    div.addEventListener("mouseover",(event)=>{
+        div.style.backgroundColor='#4284f5';
+    })
+}
+
+
+
