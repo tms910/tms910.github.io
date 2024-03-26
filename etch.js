fb5942db5b7c2a4ad92ffecafee2fdec254c95b7
tms910 <146007979+tms910@users.noreply.github.com>
2024-03-26 12:15:27 +0200

    Create div prompt button

--- etch.js
+++ etch.js
@@ -1,17 +1,29 @@
 const container=document.createElement('div');
-container.setAttribute('class','container');
-document.body.appendChild(container);
-const i=1;
+const button=document.createElement('button');
 
-for(let i=0; i<16; i++){
-    const div=document.createElement('div');
-    div.setAttribute('class','child');
-    container.appendChild(div);
-    
-    div.addEventListener("mouseover",(event)=>{
-        div.style.backgroundColor='#4284f5';
-    })
-}
+button.textContent='Square Creator';
+button.addEventListener('click',(event)=>{
+    let i=prompt('Type how many squares do you want to create:\n(No more than 99 squares)');
+    for(i; i>0 && i<100; i--){
+        const div=document.createElement('div');
+        div.setAttribute('class',`child${i}`);
+        container.appendChild(div);
+        
+        div.addEventListener("mouseover",(event)=>{
+            div.style.backgroundColor='#4284f5';
+        })        
+    }
+})
+
+
+container.setAttribute('class','container');
+document.body.appendChild(button);
+document.body.appendChild(container);
+
+
+
+
+
 
 
 
