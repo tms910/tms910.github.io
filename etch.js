b1a87785e3b25baa94fc9f67ce6b9cf6b94e0c0b
tms910 <146007979+tms910@users.noreply.github.com>
2024-03-31 04:55:54 +0300

    Create starting 16x16 grid, change divs size to auto and move button to bottom

--- etch.js
+++ etch.js
@@ -1,25 +1,49 @@
 const container=document.createElement('div');
 const button=document.createElement('button');
+let i='';
 
-button.textContent='Square Creator';
+button.textContent='Click here create your own grid';
+container.style.visibility='hidden';
+
+container.textContent='';
+container.style.width=`500px`;
+
+for(let b=0; b<16**2; b++){
+    container.style.visibility='visible';
+    const div=document.createElement('div');
+    div.setAttribute('class',`child${b}`);
+    div.style.height=`${500/16-1.6}px`;
+    div.style.width=`${500/16-1.6}px`;
+    container.appendChild(div);
+    div.addEventListener("mouseover",(event)=>{
+        div.style.backgroundColor='#00BFFF';
+    })        
+}
+
+
 button.addEventListener('click',(event)=>{
-    let i=prompt('Type how many squares do you want to create:\n(No more than 99 squares)');
+    i=prompt('Type squares per side:\n(No more than 100)');
     container.textContent='';
-    for(i; i>0 && i<100; i--){
+    for(a=i**2; a>0 && i<=100 ; a--){
+        container.style.visibility='visible';
         const div=document.createElement('div');
-        div.setAttribute('class',`child${i}`);
+        div.setAttribute('class',`child${a}`);
+        div.style.height=`${500/i-1.6}px`;
+        div.style.width=`${500/i-1.6}px`;
         container.appendChild(div);
         
         div.addEventListener("mouseover",(event)=>{
-            div.style.backgroundColor='#4284f5';
+            div.style.backgroundColor='#00BFFF';
         })        
     }
 })
 
 
 container.setAttribute('class','container');
-document.body.appendChild(button);
+
 document.body.appendChild(container);
+document.body.appendChild(button);
+
 
 
 
